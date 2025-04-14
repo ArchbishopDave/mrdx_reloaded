f79eea07288e39b62b2b17725de4478a07ecb476
ArchbishopDave <ArchbishopDave@gmail.com>
2025-04-13 21:11:42 -0600

    Base Mod Version Increment

--- PublishAll.ps1
+++ PublishAll.ps1
@@ -6,7 +6,7 @@ Split-Path $MyInvocation.MyCommand.Path | Push-Location
 
 # Name of interface mod, version
 # These are kinda hard coded into the publish script for now.
-$BaseModInterfaceVersion = "1.1.3"
+$BaseModInterfaceVersion = "1.1.4"
 $InterfaceVersions = @{
     BaseExtractDataBinInterface = "1.1.2";
     BaseModInterfaces = $BaseModInterfaceVersion
